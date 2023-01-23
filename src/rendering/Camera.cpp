2e5328d81433e7bccaf64e7c36a6f61f564337f7
janla <jan@lafferton.de>
2023-01-23 17:03:04 +0100

    optimioze etc

--- src/rendering/Camera.cpp
+++ src/rendering/Camera.cpp
@@ -9,6 +9,12 @@ glm::mat4 calculateProjection(int screenWidth, int screenHeight) {
         0.1f,
         100.0f
     );
+    //float halfW = float(screenWidth) / 2.0f;
+    //float halfH = float(screenHeight) / 2.0f;
+    //return glm::ortho(-halfW, halfW, -halfH, halfH);
+    //float w = float(screenWidth);
+    //float h = float(screenHeight);
+    //return glm::ortho(0.0f, w, 0.0f, h, 0.1f, 100.0f);
 }
 
 glm::mat4 calculateView(Camera& camera) {
@@ -17,4 +23,10 @@ glm::mat4 calculateView(Camera& camera) {
         camera.target,
         glm::vec3(0, 1, 0)
     );
+
+    /*return glm::lookAt(
+            {63, 63, 63},
+            {0, 0, 0},
+            glm::vec3(0, 1, 0)
+            );*/
 }
