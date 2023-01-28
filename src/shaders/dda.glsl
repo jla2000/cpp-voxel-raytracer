8b7b67770f75006594792fe98c84574b97bd5e45
janla <jan@lafferton.de>
2023-01-28 22:25:34 +0100

    use branchless dda

--- src/shaders/dda.glsl
+++ src/shaders/dda.glsl
@@ -15,35 +15,35 @@ void initDDA(out DDA dda, vec3 rayPos, vec3 rayDir) {
 }
 
 void iterDDA(inout DDA dda) {
-    //bvec3 mask = lessThanEqual(dda.sideDist.xyz, min(dda.sideDist.yzx, dda.sideDist.zxy));
-    //dda.sideDist += vec3(mask) * dda.deltaDist;
-    //dda.pos += vec3(mask) * dda.rayStep;
-    //dda.normal = vec3(mask) * -dda.rayStep;
-    // FIXME: branchless dda normals
+    bvec3 mask = lessThanEqual(dda.sideDist.xyz, min(dda.sideDist.yzx, dda.sideDist.zxy));
+    dda.dist = length(vec3(mask) * dda.sideDist);
+    dda.sideDist += vec3(mask) * dda.deltaDist;
+    dda.pos += vec3(mask) * dda.rayStep;
+    dda.normal = vec3(mask) * - dda.rayStep;
 
-    if (dda.sideDist.x < dda.sideDist.y) {
-        if (dda.sideDist.z < dda.sideDist.x) {
-            dda.dist = dda.sideDist.z;
-            dda.sideDist.z += dda.deltaDist.z;
-            dda.pos.z += dda.rayStep.z;
-            dda.normal = vec3(0, 0,-dda.rayStep.z);
-        } else {
-            dda.dist = dda.sideDist.x;
-            dda.sideDist.x += dda.deltaDist.x;
-            dda.pos.x += dda.rayStep.x;
-            dda.normal = vec3(-dda.rayStep.x, 0, 0);
-        }
-    } else {
-        if (dda.sideDist.z < dda.sideDist.y) {
-            dda.dist = dda.sideDist.z;
-            dda.sideDist.z += dda.deltaDist.z;
-            dda.pos.z += dda.rayStep.z;
-            dda.normal = vec3(0, 0,-dda.rayStep.z);
-        } else {
-            dda.dist = dda.sideDist.y;
-            dda.sideDist.y += dda.deltaDist.y;
-            dda.pos.y += dda.rayStep.y;
-            dda.normal = vec3(0, -dda.rayStep.y, 0);
-        }
-    }
+    //if (dda.sideDist.x < dda.sideDist.y) {
+    //    if (dda.sideDist.z < dda.sideDist.x) {
+    //        dda.dist = dda.sideDist.z;
+    //        dda.sideDist.z += dda.deltaDist.z;
+    //        dda.pos.z += dda.rayStep.z;
+    //        dda.normal = vec3(0, 0, - dda.rayStep.z);
+    //    } else {
+    //        dda.dist = dda.sideDist.x;
+    //        dda.sideDist.x += dda.deltaDist.x;
+    //        dda.pos.x += dda.rayStep.x;
+    //        dda.normal = vec3(- dda.rayStep.x, 0, 0);
+    //    }
+    //} else {
+    //    if (dda.sideDist.z < dda.sideDist.y) {
+    //        dda.dist = dda.sideDist.z;
+    //        dda.sideDist.z += dda.deltaDist.z;
+    //        dda.pos.z += dda.rayStep.z;
+    //        dda.normal = vec3(0, 0, - dda.rayStep.z);
+    //    } else {
+    //        dda.dist = dda.sideDist.y;
+    //        dda.sideDist.y += dda.deltaDist.y;
+    //        dda.pos.y += dda.rayStep.y;
+    //        dda.normal = vec3(0, - dda.rayStep.y, 0);
+    //    }
+    //}
 }
